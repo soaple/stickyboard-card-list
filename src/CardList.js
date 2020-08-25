c047de0a54a03f82d86c69d6c9cbec4440cc4f91
taeeun kim <kimtaeeeny@gmail.com>
2020-08-25 21:56:12 +0900

    add dummy card list

--- src/CardList.js
+++ src/CardList.js
@@ -4,17 +4,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    height: 100%;
+    padding: 8px;
+    overflow: scroll;
+`;
 
 class CardList extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            list: [
+                'test1',
+                'test2',
+                'test3',
+                'test4',
+                'test5',
+                'test6',
+                'test7',
+                'test8',
+                'test9',
+                'test10',
+            ],
+        };
     }
 
     render() {
-        return <Wrapper></Wrapper>;
+        return (
+            <Wrapper>
+                {this.state.list.map((item) => {
+                    return (
+                        <div
+                            style={{
+                                border: '1px solid red',
+                                margin: '6px',
+                                padding: '6px',
+                                fontSize: '20px',
+                                borderCollapse: 'collapse',
+                            }}>
+                            {item}
+                        </div>
+                    );
+                })}
+            </Wrapper>
+        );
     }
 }
 
