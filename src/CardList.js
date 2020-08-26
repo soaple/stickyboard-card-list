30757fbe13e362f43a19479703660cdfa2d80154
taeeun kim <kimtaeeeny@gmail.com>
2020-08-26 19:46:23 +0900

    add component layout

--- src/CardList.js
+++ src/CardList.js
@@ -3,6 +3,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import {
+    AiOutlineComment,
+    AiOutlineEye,
+    AiOutlineShareAlt,
+    AiOutlineHeart,
+} from 'react-icons/ai';
 
 const Wrapper = styled.div`
     height: 100%;
@@ -36,36 +42,114 @@ class CardList extends React.Component {
         console.log(data);
         return (
             <Wrapper>
-                <div style={{ borderBottom: '1px solid black' }}>{title}</div>
+                <div
+                    style={{
+                        borderBottom: '3px double black',
+                        fontFamily: 'Noto Sans KR',
+                        fontWeight: '900',
+                    }}>
+                    {title}
+                </div>
                 {data.map((item) => {
                     console.log(item);
                     return (
                         <div
                             style={{
-                                border: '1px solid red',
+                                borderBottom: '1px solid grey',
                                 margin: '6px',
                                 padding: '6px',
+                                backgroundColor: '#f7f7f7',
                             }}>
-                            <div style={{ fontSize: '16px' }}>
-                                <span
+                            <div
+                                style={{
+                                    fontSize: '16px',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                }}>
+                                <div
                                     style={{
                                         backgroundColor: item.badgeColor,
                                         color: 'white',
+                                        borderRadius: '5px',
+                                        fontSize: '10px',
+                                        fontWeight: 'bold',
+                                        padding: '2px',
                                     }}>
                                     공지
-                                </span>
-                                {item.title}
+                                </div>
+                                &nbsp;
+                                <div
+                                    style={{
+                                        fontSize: '14px',
+                                        fontWeight: 'bold',
+                                    }}>
+                                    {item.title}
+                                </div>
                             </div>
-                            <div style={{ fontSize: '12px' }}>
+                            <div
+                                style={{
+                                    fontSize: '12px',
+                                    marginTop: '5px',
+                                    marginBottom: '10px',
+                                }}>
                                 {item.description}
                             </div>
-                            <div>
-                                <span>{item.name}</span>
-                                <span>{item.date}</span>
-                                <span>comment:{item.comment}</span>
-                                <span>view{item.view}</span>
-                                <span>share{item.share}</span>
-                                <span>like{item.like}</span>
+                            <div style={{ display: 'flex' }}>
+                                <div
+                                    style={{
+                                        fontSize: '12px',
+                                        fontWeight: 'bold',
+                                    }}>
+                                    {item.name}&nbsp;&nbsp;
+                                </div>
+                                <div
+                                    style={{
+                                        fontSize: '12px',
+                                        fontWeight: 'bold',
+                                        color: 'grey',
+                                    }}>
+                                    {item.date}&nbsp;&nbsp;
+                                </div>
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        fontSize: '12px',
+                                    }}>
+                                    <AiOutlineComment />
+                                    &nbsp;
+                                    <div>{item.comment}</div>&nbsp;
+                                </div>
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        fontSize: '12px',
+                                    }}>
+                                    <AiOutlineEye />
+                                    &nbsp;
+                                    <div>{item.view}</div>&nbsp;
+                                </div>
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        fontSize: '12px',
+                                    }}>
+                                    <AiOutlineShareAlt />
+                                    &nbsp;
+                                    <div>{item.view}</div>&nbsp;
+                                </div>
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        fontSize: '12px',
+                                    }}>
+                                    <AiOutlineHeart />
+                                    &nbsp;
+                                    <div>{item.view}</div>&nbsp;
+                                </div>
                             </div>
                         </div>
                     );
